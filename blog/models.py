0852a03b136f94dfe1674c7ca45d81b92724c3c9
CharlottaG <DELETED-1691214276921>
2024-03-22 16:08:31 +0000

    Add Comment functionality

--- blog/models.py
+++ blog/models.py
@@ -9,10 +9,34 @@ LEVEL = ((0, "Beginner"), (1, "Intermediate"), (2, "Advanced"))
 class Pattern(models.Model):
     pattern_name = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
-    created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_posts")
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name="patterncreator")
     instructions = models.TextField()
-    needle_size = models.CharField(max_length=20)
-    gauge = models.CharField(max_length=20)
+    needle_size = models.CharField(max_length=50)
+    gauge = models.CharField(max_length=50)
+    yarn = models.CharField(max_length=50, default="yarn")
     difficulity_level = models.IntegerField(choices=LEVEL,)
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ["created_on"]
+
+
+    def __str__(self):
+        return f"{self.pattern_name} | by {self.created_by}"
+
+
+class Comment(models.Model):
+    pattern = models.ForeignKey(Pattern, on_delete=models.CASCADE, related_name="comments")
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="commenter")
+    body = models.TextField()
+    approved = models.BooleanField(default=False)
+    created_on = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        ordering = ["created_on"]
+
+    def __str__(self):
+        return f"Comment: {self.body} | by {self.author}"
+
