9b3f2f495a8bb5ea0e4a4243e27732a84dc16078
CharlottaG <DELETED-1691214276921>
2024-03-24 15:06:56 +0000

    Fix issue with viewing pattern details

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views import generic
 from django.views.generic.detail import DetailView
+from django.shortcuts import render, get_object_or_404
 from .models import Pattern
 
 # Create your views here.
@@ -10,13 +11,7 @@ class PatternList(generic.ListView):
     template_name = "blog/index.html"
     paginate_by = 6
 
-class PatternDetail(DetailView):
-    model = Pattern
-    template_name = 'pattern_details.html' 
-    context_object_name = 'pattern'
 
-from django.shortcuts import render, get_object_or_404
-from .models import Pattern
 
 
 def pattern_details(request, slug):
