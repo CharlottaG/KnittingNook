3654768a8b2b24e830d6fc8af74e8cfbf6531f86
CharlottaG <DELETED-1691214276921>
2024-03-24 15:02:00 +0000

    Add function to view pattern details

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.PatternList.as_view(), name='home'),
+    path('<slug:slug>/', views.PatternDetail.as_view(), name="pattern_details"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views import generic
+from django.views.generic.detail import DetailView
 from .models import Pattern
 
 # Create your views here.
@@ -8,3 +9,25 @@ class PatternList(generic.ListView):
     queryset = Pattern.objects.filter(status=1)
     template_name = "blog/index.html"
     paginate_by = 6
+
+class PatternDetail(DetailView):
+    model = Pattern
+    template_name = 'pattern_details.html' 
+    context_object_name = 'pattern'
+
+from django.shortcuts import render, get_object_or_404
+from .models import Pattern
+
+
+def pattern_details(request, slug):
+    queryset = Pattern.objects.filter(status=1)
+    pattern = get_object_or_404(queryset, slug=slug)
+
+    return render(
+        request,
+        "blog/pattern_details.html",
+        {
+            "pattern": pattern,
+          
+        },
+    )
